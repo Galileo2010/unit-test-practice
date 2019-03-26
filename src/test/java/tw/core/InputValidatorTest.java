977db332846825ae68266d514330fc7306b0ca79
Galileo2010 <15549070600@163.com>
2019-03-26 17:49:58 +0800

    add some test for InoutValidator and RandomIntGen

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,22 @@
 package tw.core;
 
+import org.junit.jupiter.api.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
-
+    @Test
+    void validate(){
+        InputValidator input = new InputValidator();
+        assertTrue(input.validate("1 2 3 4"));
+        assertFalse(input.validate("12 3 4"));
+        assertFalse(input.validate("1 23 4"));
+        assertFalse(input.validate("1 2"));
+        assertFalse(input.validate("1 2 3 40"));
+    }
 }
--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,22 @@
 package tw.core;
 
 
+import org.junit.jupiter.api.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
-
+    @Test
+    void generateNums(){
+        RandomIntGenerator randomint = new RandomIntGenerator();
+        assertTrue(randomint.generateNums(9,4).matches("[0-9] [0-9] [0-9] [0-9]"));
+        assertTrue(randomint.generateNums(5,4).matches("[0-5] [0-5] [0-5] [0-5]"));
+        assertTrue(randomint.generateNums(6,3).matches("[0-6] [0-6] [0-6]"));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> randomint.generateNums(3,4));
+        assertEquals("Can't ask for more numbers than are available", exception.getMessage());
+    }
 }
